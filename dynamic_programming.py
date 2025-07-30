2c6dec741e2e95eab8195cad84aa67f0734b39dc
Ibrahim <ibrahimzaky2000@gmail.com>
2025-07-30 06:41:15 +0200

    Initial commit of dynamic programming problems

--- dynamic_programming.py
+++ dynamic_programming.py
@@ -1,22 +1,165 @@
-def can_sum(target_sum, numbers, memo=None):
+def can_construct(target, word_bank, memo=None):
     if memo is None:
         memo = {}
 
-    if target_sum in memo:
-        return memo[target_sum]
-    if target_sum == 0:
+    if target in memo:
+        return memo[target]
+    if target == "":
         return True
-    if target_sum < 0:
-        return False
 
-    for num in numbers:
-        remainder = target_sum - num
-        if can_sum(remainder, numbers, memo):
-            memo[target_sum] = True
-            return True
+    for word in word_bank:
+        if target.startswith(word):
+            suffix = target[len(word):]
+            if can_construct(suffix, word_bank, memo):
+                memo[target] = True
+                return True
 
-    memo[target_sum] = False
+    memo[target] = False
     return False
+
+
+print(can_construct("abcdef", ["ab", "abc", "cd", "def", "abcd"]))
+print(can_construct("skateboard", ["bo", "rd", "ate", "t", "ska", "sk", "boar"]))
+print(can_construct("enterapotentpot", ["a", "p", "ent", "enter", "ot", "t"]))
+print(can_construct(
+    "eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef",
+    ["e", "ee", "eee", "eeee", "eeeee", "eeeeee"]
+))
+
+
+
+
+
+
+
+
+
+def count_construct(target, word_bank, memo=None):
+    if memo is None:
+        memo = {}
+
+    if target in memo:
+        return memo[target]
+    if target == "":
+        return 1
+
+    total_count = 0
+
+    for word in word_bank:
+        if target.startswith(word):
+            num_ways_for_rest = count_construct(target[len(word):], word_bank, memo)
+            total_count += num_ways_for_rest
+
+    memo[target] = total_count
+    return total_count
+
+
+print(count_construct('purple', ['purp', 'p', 'ur', 'le', 'purpl']))
+print(count_construct("abcdef", ["ab", "abc", "cd", "def", "abcd"]))
+print(count_construct("skateboard", ["bo", "rd", "ate", "t", "ska", "sk", "boar"]))
+print(count_construct("enterapotentpot", ["a", 'p', "ent", "enter", "ot", "o", "t"]))
+print(count_construct("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef", [
+    "e", 
+    "ee", 
+    "eee",
+    "eeee", 
+    "eeeee",
+    "eeeeee"
+]))
+
+
+
+
+
+def all_construct(target, word_bank, memo=None):
+    if memo is None:
+        memo = {}
+
+    if target in memo:
+        return memo[target]
+    if target == "":
+        return [[]]
+
+    result = []
+
+    for word in word_bank:
+        if target.startswith(word):
+            suffix = target[len(word):]
+            suffix_ways = all_construct(suffix, word_bank, memo)
+            target_ways = [[word] + way for way in suffix_ways]
+            result.extend(target_ways)
+
+    memo[target] = result
+    return result
+
+
+
+print(all_construct('purple', ['purp', 'p', 'ur', 'le', 'purpl']))
+print(all_construct("abcdef", ["ab", "abc", "cd", "def", "abcd", "ef", "c"]))
+print(all_construct("skateboard", ["bo", "rd", "ate", "t", "ska", "sk", "boar"]))
+print(all_construct("aaaaaaaaaaaaaaaaaaaaaaaaaaz", ["a", "aa", "aaa", "aaaa", "aaaaa"]))
+
+
+
+
+
+def fib(n):
+    if n == 0:
+        return 0
+    table = [0] * (n + 2)  
+    table[1] = 1
+    for i in range(n):
+        table[i + 1] += table[i]
+        table[i + 2] += table[i]
+    return table[n]
+
+print(fib(6))
+print(fib(7))
+print(fib(8))
+print(fib(50))
+
+
+
+
+
+
+
+
+def grid_traveler(m, n):
+    table = [[0] * (n + 1) for _ in range(m + 1)]
+    table[1][1] = 1
+
+    for i in range(m + 1):
+        for j in range(n + 1):
+            current = table[i][j]
+            if j + 1 <= n:
+                table[i][j + 1] += current
+            if i + 1 <= m:
+                table[i + 1][j] += current
+
+    return table[m][n]
+
+
+print(grid_traveler(1, 1))
+print(grid_traveler(2, 3))
+print(grid_traveler(3, 2))
+print(grid_traveler(3, 3))
+print(grid_traveler(18, 18))
+
+
+
+
+def can_sum(target_sum, numbers):
+    table = [False] * (target_sum + 1)
+    table[0] = True
+
+    for i in range(target_sum + 1):
+        if table[i]:
+            for num in numbers:
+                if i + num <= target_sum:
+                    table[i + num] = True
+
+    return table[target_sum]
 
 
 print(can_sum(7, [2, 3]))          
@@ -24,68 +167,3 @@ print(can_sum(7, [5, 3, 4, 7]))
 print(can_sum(7, [2, 4]))          
 print(can_sum(8, [2, 3, 5]))       
 print(can_sum(300, [7, 14]))       
-
-
-
-
-
-
-
-
-
-
-def how_sum(target_sum, numbers, memo=None):
-    if memo is None:
-        memo = {}
-
-    if target_sum in memo:
-        return memo[target_sum]
-    if target_sum == 0:
-        return []
-    if target_sum < 0:
-        return None
-
-    for num in numbers:
-        remainder = target_sum - num
-        remainder_result = how_sum(remainder, numbers, memo)
-        if remainder_result is not None:
-            memo[target_sum] = remainder_result + [num]
-            return memo[target_sum]
-
-    memo[target_sum] = None
-    return None
-
-
-print(how_sum(7, [2, 3]))          
-print(how_sum(7, [5, 3, 4, 7]))    
-print(how_sum(7, [2, 4]))          
-print(how_sum(8, [2, 3, 5]))       
-print(how_sum(300, [7, 14]))       
-
-
-
-
-
-
-def best_sum(target_sum, numbers):
-    if target_sum == 0:
-        return []
-    if target_sum < 0:
-        return None
-
-    shortest_combination = None
-
-    for num in numbers:
-        remainder = target_sum - num
-        remainder_combination = best_sum(remainder, numbers)
-        if remainder_combination is not None:
-            combination = remainder_combination + [num]
-            if shortest_combination is None or len(combination) < len(shortest_combination):
-                shortest_combination = combination
-
-    return shortest_combination
-
-
-print(best_sum(7, [5, 3, 4, 7]))     
-print(best_sum(8, [2, 3, 5]))        
-print(best_sum(8, [1, 4, 5]))        
